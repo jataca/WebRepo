a1e621b2a0758cea6396825ad1daff7416a845a0
Jacob Casperson <jtannercasperson@gmail.com>
2015-06-05 02:54:57 -0600

    edtis

--- welcome.php
+++ welcome.php
@@ -3,9 +3,9 @@
   <?php
   $myfile = fopen("testfile.txt", "w") or die("Unable to open file!");
   fwrite($myfile, $_POST["name"]);
+  fclose($myfile);
   ?>
   Welcome <?php echo $_POST["name"]; ?><br>
   Your email address is: <?php echo $_POST["email"]; ?>
-  fclose($myfile);
 </body>
 </html>
