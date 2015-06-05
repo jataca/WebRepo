b4208bd3ac748a0ceff037e7ce1d2d4d7536569d
Jacob Casperson <jtannercasperson@gmail.com>
2015-06-05 03:12:57 -0600

    try to read a file

--- welcome.php
+++ welcome.php
@@ -1,13 +1,20 @@
+<!DOCTYPE html>
 <?php
-$myfile = fopen("testfile.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $_POST["name"]);
-fwrite($myfile, "WHAT");
-fclose($myfile);
+  $myfile = fopen("testfile.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $_POST["name"]);
+  fwrite($myfile, "WHAT");
+  fclose($myfile);
 ?>
 <html>
 <body>
 
   Welcome <?php echo $_POST["name"]; ?><br>
   Your email address is: <?php echo $_POST["email"]; ?>
+
+  <?php
+  $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+  echo fread($myfile,filesize("webdictionary.txt"));
+  fclose($myfile);
+  ?>
 </body>
 </html>
