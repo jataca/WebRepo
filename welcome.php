2ffbf75f6180cbcb15278c1193f574eb7df0d50a
Jacob Casperson <jtannercasperson@gmail.com>
2015-06-05 02:47:52 -0600

    write name to file

--- welcome.php
+++ welcome.php
@@ -1,6 +1,11 @@
 <html>
 <body>
+  <?php
+  $myfile = fopen("testfile.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $_POST["name"]);
+  ?>
   Welcome <?php echo $_POST["name"]; ?><br>
   Your email address is: <?php echo $_POST["email"]; ?>
+  fclose($myfile);
 </body>
 </html>
