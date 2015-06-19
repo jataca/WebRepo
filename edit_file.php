acf4c1ffc9790642b84fe3e705517366fa0eb561
Jacob Casperson <jtannercasperson@gmail.com>
2015-06-18 21:16:13 -0700

    added new assignment for edit_file

---
+++ edit_file.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="homestyles.css">
+  </head>
+  <body>
+    <div>
+      <form action="welcome.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+      </form>
+    </div>
+  </body>
+</html>
